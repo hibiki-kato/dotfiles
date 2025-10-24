a8d27358c9f8b6da8c7c8ed996c2d31a1962a011
hibiki-kato <katohibiki17@gmail.com>
2025-10-24 19:58:45 -0400

    apt-getを使用してBraveブラウザとVSCodeのインストールスクリプトを更新しました

--- install/ubuntu/desktop/40-warp.sh
+++ install/ubuntu/desktop/40-warp.sh
@@ -2,4 +2,5 @@ wget -qO- https://releases.warp.dev/linux/keys/warp.asc | gpg --dearmor > warpdo
 sudo install -D -o root -g root -m 644 warpdotdev.gpg /etc/apt-get/keyrings/warpdotdev.gpg
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt-get/keyrings/warpdotdev.gpg] https://releases.warp.dev/linux/deb stable main" > /etc/apt-get/sources.list.d/warpdotdev.list'
 rm -f warpdotdev.gpg
+sudo apt-get update
 sudo apt-get install -y warp-terminal
