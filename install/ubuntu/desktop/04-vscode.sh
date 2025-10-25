92b9ac3c6464cc8a27541bf2c3da7fd2c0bb7a4f
hibiki-kato <katohibiki17@gmail.com>
2025-10-24 20:09:03 -0400

    vscode.shとwarp.shのスクリプトを修正し、GPGキーの削除処理を整理しました。

--- install/ubuntu/desktop/04-vscode.sh
+++ install/ubuntu/desktop/04-vscode.sh
@@ -3,3 +3,4 @@ sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update
 sudo apt-get install -y code
+rm -f packages.microsoft.gpg
--- install/ubuntu/desktop/06-warp.sh
+++ install/ubuntu/desktop/06-warp.sh
@@ -1,6 +1,6 @@
 wget -qO- https://releases.warp.dev/linux/keys/warp.asc | gpg --dearmor > warpdotdev.gpg
 sudo install -D -o root -g root -m 644 warpdotdev.gpg /etc/apt-get/keyrings/warpdotdev.gpg
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt-get/keyrings/warpdotdev.gpg] https://releases.warp.dev/linux/deb stable main" > /etc/apt-get/sources.list.d/warpdotdev.list'
-rm -f warpdotdev.gpg
 sudo apt-get update
 sudo apt-get install -y warp-terminal
+rm -f warpdotdev.gpg
