5ce460248a3dbe770ddb3ab021b1fb89d30f72bf
hibiki-kato <katohibiki17@gmail.com>
2025-10-24 20:06:06 -0400

    Google Chromeのインストールスクリプトを修正し、wgetからcurlに変更しました。また、GPGキーの保存先を更新しました。

--- install/ubuntu/desktop/01-chrome.sh
+++ install/ubuntu/desktop/01-chrome.sh
@@ -1,5 +1,4 @@
-wget https://dl-ssl.google.com/linux/linux_signing_key.pub -O /tmp/google.pub
-gpg --no-default-keyring --keyring /etc/apt/keyrings/google-chrome.gpg --import /tmp/google.pub
-echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get update 
-sudo apt-get install google-chrome-stable -y
+curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/google-chrome.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
