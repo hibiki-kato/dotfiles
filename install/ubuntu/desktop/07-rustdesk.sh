e2f87fb64d052ddea4917ac73cd954f014855ddc
hibiki-kato <katohibiki17@gmail.com>
2025-11-01 17:31:42 -0400

    rustdeskインストールスクリプトを削除

--- install/ubuntu/desktop/07-rustdesk.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-set -euo pipefail
-
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:pipewire-debian/pipewire-upstream
-sudo apt update
-# 誤ったディスク使用量レポートは無視してください
-sudo apt install -fy ./rustdesk-<version>.deb
