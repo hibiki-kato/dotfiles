2c868809dd4e470c26e62482e00fbe45518c90c1
hibiki-kato <katohibiki17@gmail.com>
2025-11-04 16:23:47 -0500

    rustdeskインストールスクリプトを更新し、Flatpakを使用してRustDeskをインストールするように変更しました

---
+++ install/ubuntu/desktop/07-rustdesk.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+set -euo pipefail
+
+sudo apt-get -y install flatpak \
+    gnome-software-plugin-flatpak
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub com.rustdesk.RustDesk
---
+++ install/ubuntu/desktop/09-tailscale.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+set -euo pipefail
+
+
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+sudo apt-get -y update 
+sudo apt-get -y install tailscale
