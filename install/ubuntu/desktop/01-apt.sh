d8e00d0bf6415170536014f1aabbf5f214325873
hibiki-kato <katohibiki17@gmail.com>
2026-02-09 15:52:39 -0500

    autostart用の.desktopファイルを追加し、aptスクリプトにcopyqをインストールするように変更しました

--- install/ubuntu/desktop/01-apt.sh
+++ install/ubuntu/desktop/01-apt.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 sudo apt-get update -y || true
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
   stacer \
-  indicator-multiload
-  trash-cli
+  indicator-multiload \
+  trash-cli \
+  copyq
 
