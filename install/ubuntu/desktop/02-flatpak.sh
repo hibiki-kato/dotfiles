b5af8075ffd493e64f45e00db5b786fc53653207
hibiki-kato <katohibiki17@gmail.com>
2026-02-11 14:59:53 -0500

    flatpakスクリプトからOpenRGBのインストールをコメントアウトし、udevルールのダウンロード手順をコメント化しました

--- install/ubuntu/desktop/02-flatpak.sh
+++ install/ubuntu/desktop/02-flatpak.sh
@@ -7,4 +7,4 @@ sudo apt-get -y install flatpak \
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub com.rustdesk.RustDesk
 flatpak install -y flathub io.github.jeffshee.Hidamari
-flatpak install -y flathub org.openrgb.OpenRGB
+# flatpak install -y flathub org.openrgb.OpenRGB
--- install/ubuntu/desktop/11-openrgb.sh
+++ install/ubuntu/desktop/11-openrgb.sh
@@ -1,17 +1,17 @@
 #! /bin/zsh
-set -euo pipefail
+# set -euo pipefail
 
-# OpenRGB was installed via flatpak.
-# Download udev rules file
-wget https://openrgb.org/releases/release_0.9/60-openrgb.rules
+# # OpenRGB was installed via flatpak.
+# # Download udev rules file
+# wget https://openrgb.org/releases/release_0.9/60-openrgb.rules
 
-# Move udev rules file to udev rules directory
-sudo mv 60-openrgb.rules /usr/lib/udev/rules.d
+# # Move udev rules file to udev rules directory
+# sudo mv 60-openrgb.rules /usr/lib/udev/rules.d
 
-# Reload the rules
-sudo udevadm control --reload-rules
-sudo udevadm trigger
+# # Reload the rules
+# sudo udevadm control --reload-rules
+# sudo udevadm trigger
 
-# Also, load script
-systemctl --user daemon-reload
-systemctl --user enable --now rgb-load.timer
+# # Also, load script
+# systemctl --user daemon-reload
+# systemctl --user enable --now rgb-load.timer
